4a285ce65120555e6c1f0a796119c9d0be292e7c
kokonoe <130939038+nonuplet@users.noreply.github.com>
2026-02-23 11:57:39 +0900

    rootコマンドのコメント追加・エラー時にos.Exit(1)するように実装変更

--- cmd/root.go
+++ cmd/root.go
@@ -2,18 +2,23 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "myapp",
-	Short: "My CLI tool",
+	Use:   "archon",
+	Short: "ゲームサーバ管理用CLIツール",
+	Long:  "ゲームサーバのインストール、セーブデータバックアップなどの操作を提供するCLIツールです。",
 }
 
 // Execute execute
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
-		return
+		fmt.Fprintf(os.Stderr, "error: %v", err)
+		os.Exit(1)
 	}
 }
